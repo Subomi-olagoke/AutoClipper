78eaa855154c1e7f3e7c98387ec4157d89d89de2
Harrison owede <harrisonowede3@gmail.com>
2025-11-13 13:05:42 +0100

    changing url

--- src/routes/commentRoutes.js
+++ src/routes/commentRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 let commentCount = 0;
 let baselineComments = 10; // baseline for simulation
-const streamUrl = "https://www.twitch.tv/dakotaz"; // example stream
+const streamUrl = "https://www.twitch.tv/loeya"; // example stream
 
 router.get("/spike", (req, res) => {
   commentCount += Math.floor(Math.random() * 20); // simulate incoming comments
